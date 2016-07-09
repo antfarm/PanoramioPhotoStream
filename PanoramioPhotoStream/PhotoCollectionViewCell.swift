08b6c74635f609bc267a6f71de9d10d56fab2293
sean <sean@antfarm.de>
2016-07-09 15:04:12 +0200

    Layout with dummy image.

---
+++ PanoramioPhotoStream/PhotoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PanoramioPhotoStream
+//
+//  Created by sean on 09/07/16.
+//  Copyright © 2016 antfarm. All rights reserved.
+//
+
+import UIKit
+
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var photoImageView: UIImageView!
+
+    static let reuseId = "Photo Collection View Cell"
+}
--- PanoramioPhotoStream/PhotoStreamViewController.swift
+++ PanoramioPhotoStream/PhotoStreamViewController.swift
@@ -8,18 +8,76 @@
 
 import UIKit
 
+
 class PhotoStreamViewController: UIViewController {
 
+    @IBOutlet var startButtonItem: UIBarButtonItem!
+    @IBOutlet var stopButtonItem: UIBarButtonItem!
+
+    @IBOutlet var photoCollectionView: UICollectionView!
+
+
+    let dummyImage = UIImage(named:"gt40_rhinluch.jpg")!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        photoCollectionView.dataSource = self
+        photoCollectionView.delegate = self
     }
 
 
+    @IBAction func startStreaming(sender: UIBarButtonItem) {
+
+        startButtonItem.enabled = false
+        stopButtonItem.enabled = true
+
+        print("Starting ...")
+    }
+
+
+    @IBAction func stopStreaming(sender: UIBarButtonItem) {
+
+        stopButtonItem.enabled = false
+        startButtonItem.enabled = true
+
+        print("Stopping ...")
+    }
 }
+
+
+extension PhotoStreamViewController: UICollectionViewDataSource {
+
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(PhotoCollectionViewCell.reuseId, forIndexPath: indexPath) as! PhotoCollectionViewCell
+
+        cell.photoImageView.image = dummyImage
+
+        return cell
+    }
+
+
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return 10
+    }
+}
+
+
+extension PhotoStreamViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        let lineSpacing = flowLayout.minimumLineSpacing // set in Interface BUilder
+
+        let width = collectionView.bounds.width - 2 * lineSpacing
+        let height = dummyImage.size.height * (width / dummyImage.size.width)
+
+        return CGSize(width: width, height: height)
+    }
+}
+
 
